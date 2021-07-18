2c89fcc28db3ebd757a97a29aebb9abb2fc09c6a
Alan Cho <alancho@Alans-MBP.fios-router.home>
2021-07-18 13:17:32 -0400

    add My to Memories

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     <Container maxWidth='lg'>
       <AppBar className={classes.appBar} position='static' color='inherit'>
         <Typography className={classes.heading} variant='h2' align='center'>
-          Memories
+          My Memories
         </Typography>
         <img
           className={classes.image}
