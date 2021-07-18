fd4a499b63a4d6fe361015c58491ac5a913e6c5b
Alan Cho <alancho@Alans-MBP.fios-router.home>
2021-07-18 14:52:10 -0400

    heroku deployment successful

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello to memories API");
+  res.send("Hello again to memories API");
 });
 
 const PORT = process.env.PORT || 5000;
